e4017e9038196e629f62451eef2b3c6be97fff5c
José Luis González Beltrán <joseluis.gonzalezbeltran@externals.adidas-group.com>
2019-02-18 10:33:32 +0100

    (feature) generate docker image with Makefile with jar file copied inside image

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mvn -f ./pom.xml clean package dockerfile:build
+export PROJECT_VERSION=0.0.1-SNAPSHOT
+make -C docker tag
 docker rm -f coding-challenge-route-calculator
 docker run -d -p 8070:8080 \
     --name coding-challenge-route-calculator \
     --net codingchallenge \
-    -v ~/projects/coding-challenge/coding-challenge-route-calculator/target:/opt/lib \
-    jgb/coding-challenge-route-calculator:latest
+    jgb/coding-challenge-route-calculator:${PROJECT_VERSION}

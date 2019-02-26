f3293ac6a6f62bf3cc68cf6b3458c7354bdb64a6
José Luis González Beltrán <joseluis.gonzalezbeltran@externals.adidas-group.com>
2019-02-26 13:58:49 +0100

    (feature) improve algorithm to support reoutes with more than two connections

--- src/main/java/org/jgb/routecalculator/service/impl/RouteCalculatorServiceImpl.java
+++ src/main/java/org/jgb/routecalculator/service/impl/RouteCalculatorServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -39,25 +40,43 @@ public class RouteCalculatorServiceImpl implements RouteCalculatorService {
         final List<RouteInfo> finalRoutesList = new ArrayList<>(candidateRoutesList);
         candidateRoutesList.forEach(candidateRoute -> {
             final CityInfo lastConnectionCityInfo = getLastConnectionCityInfo(candidateRoute);
-            final List<CityInfo> newConnectionCityInfoList =
-                    cityDataRestTemplateService.getCityList(lastConnectionCityInfo.getCityDestiny());
-
-            newConnectionCityInfoList.forEach(connection -> {
-                if (!cityOrigin.equals(connection.getCityDestiny())) {
-                    final Optional<RouteInfo> existingDestinyRouteInfo = candidateRoutesList.stream()
-                            .filter(ri -> ri.getCityDestiny().equals(connection.getCityDestiny()))
-                            .findFirst();
-
-                    RouteInfo newRouteInfo = new RouteInfo(candidateRoute, connection);
-                    if (existingDestinyRouteInfo.isPresent()) {
-                        finalRoutesList.remove(existingDestinyRouteInfo.get());
-                        newRouteInfo = strategy.compareRoutes(existingDestinyRouteInfo.get(), newRouteInfo);
-                    }
-                    finalRoutesList.add(newRouteInfo);
-                }
-            });
+            final List<CityInfo> newConnectionCityInfoList = cityDataRestTemplateService.getCityList(lastConnectionCityInfo.getCityDestiny());
+            calculateCandidateRoutes(cityOrigin, finalRoutesList, candidateRoute, newConnectionCityInfoList, lastConnectionCityInfo);
         });
-        return finalRoutesList;
+
+        return applyStrategy(finalRoutesList, strategy);
+    }
+
+    private void calculateCandidateRoutes(String cityOrigin, List<RouteInfo> finalRoutesList, RouteInfo candidateRoute,
+                                          List<CityInfo> newConnectionCityInfoList, CityInfo cityInfo) {
+        if (newConnectionCityInfoList.isEmpty()) {
+            return;
+        }
+        newConnectionCityInfoList.stream()
+                .filter(connection -> !cityOrigin.equals(connection.getCityDestiny())
+                        && connection.getDepartureTime().isAfter(cityInfo.getArrivalTime()))
+                .forEach(connection -> {
+                    final RouteInfo newCandidateRoute = new RouteInfo(candidateRoute, connection);
+                    finalRoutesList.add(newCandidateRoute);
+                    final List<CityInfo> newConnectionCityInfoListFromConnection =
+                            cityDataRestTemplateService.getCityList(connection.getCityDestiny());
+                    calculateCandidateRoutes(
+                            cityOrigin, finalRoutesList, newCandidateRoute, newConnectionCityInfoListFromConnection, connection);
+                });
+    }
+
+    private List<RouteInfo> applyStrategy(List<RouteInfo> finalRoutesList, RouteStrategy strategy) {
+        final Set<String> destinyList = finalRoutesList.stream().map(RouteInfo::getCityDestiny).collect(Collectors.toSet());
+        return destinyList.stream()
+                .map(destiny -> {
+                    final List<RouteInfo> destinyRoutes = finalRoutesList.stream()
+                            .filter(routeInfo -> destiny.equals(routeInfo.getCityDestiny()))
+                            .collect(Collectors.toList());
+                    return destinyRoutes.size() == 1
+                            ? destinyRoutes.get(0) : destinyRoutes.stream().reduce(strategy::compareRoutes).orElse(null);
+                })
+                .filter(routeInfo -> Optional.ofNullable(routeInfo).isPresent())
+                .collect(Collectors.toList());
     }
 
     private CityInfo getLastConnectionCityInfo(RouteInfo route) {
